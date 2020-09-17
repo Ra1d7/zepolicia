519d0ea1b7a8abddb69e4284a49d314c233e59f8
Raid7 <thmrgb8@gmail.com>
2020-09-17 23:30:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,23 @@ import discord
 import requests
 import asyncio
 ban_words = ["VAC ban on record","game ban on record"]
-token = "NzU1MTY3ODM4NDMxMzQ2ODUy.X1_W2w.8hwgXUj8B-f-4_6ejzxrqrnQrx0"
-client_secret = "6NCW9OrrzUMsuIudINhaUU4gLGmw4mx3"
-client_id = "755167838431346852"
+token = "NzU1MDgzNjQwNTMwMDEwMjAy.X1-IcA.DmCiXGHhBR7jN2qh4gRK3icvgHs"
 client = discord.Client()
 def checkit(profile):
     print(f"checking {profile}")
     try:
+        prof = profile
         url=""
-        if "check!" in profile:
+        print("checking if ?check is in it")
+        if "?check" in profile:
+            print("its in it")
             prof = profile[7:]
-        else:
-            prof = profile
+            print(prof)
         if "https://" in profile:
+            print("its a url")
             url=prof
         else:
+            print("else")
             url="https://steamcommunity.com/id/{}".format(prof)
         r = requests.get(url)
         for word in ban_words:
@@ -27,7 +29,8 @@ def checkit(profile):
                 return f"Profile {prof} is clean! :white_check_mark:"
             else:
                 return "Invalid Profile ID or URL"
-    except:
+    except Exception as e:
+        print(e)
         return "Invalid Profile ID or URL"
 async def getuser(user):
     global client
@@ -40,8 +43,8 @@ async def checkfile():
     while(True):
         with open("list.txt",'r') as f:
             for line in f.readlines():
-                steamid = line.split(":")[0]
-                user= line.split(":")[1].strip("\n").strip()
+                steamid = line.split("|")[0]
+                user= line.split("|")[1].strip("\n").strip()
                 if "clean" in checkit(steamid):
                     pass
                 if "has" in checkit(steamid):
@@ -54,32 +57,32 @@ async def checkfile():
                         for i in d:
                             print(i)
                             print("=")
-                            print(f"{steamid}:{user}")
-                            if i != f"{steamid}:{user}\n":
+                            print(f"{steamid}|{user}")
+                            if i != f"{steamid}|{user}\n":
                                 f.write(i)
                         f.truncate()
-            await asyncio.sleep(3600)        #await userd.send(f"{steamid} has been banned!")
+            await asyncio.sleep(3600)        #await userd.send(f"{steamid} has been banned?")
 async def savenotify(steamid,user):
     print(f"Saving Notify with user {user} and steamid {steamid}")
     global client
     await client.wait_until_ready()
     with open("list.txt",'a') as f:
-        f.write(f"{steamid}:{user}\n")
-    print("Saved!")
+        f.write(f"{steamid}|{user}\n")
+    print("Saved?")
 @client.event
 async def on_message(message):
-    if message.content.find("!help") != -1:
-        embed = discord.Embed(title="CSGO ban checker",description=":information_source: Helpful commands to use! :information_source: ")
-        embed.add_field(name="!help",value="Shows this message")
-        embed.add_field(name="!check <steam id or URL>",value="Checks the given profile's ban status :no_entry:")
-        embed.add_field(name="!notify <steam id or URL>",value="Watch this account and get notified when it gets banned :police_officer:")
-        embed.add_field(name="feedback",value="More features will be added soon! :poop:")
+    if message.content.find("?help") != -1:
+        embed = discord.Embed(title="CSGO ban checker",description=":information_source: Helpful commands to use? :information_source: ")
+        embed.add_field(name="?help",value="Shows this message")
+        embed.add_field(name="?check <steam id or URL>",value="Checks the given profile's ban status :no_entry:")
+        embed.add_field(name="?notify <steam id or URL>",value="Watch this account and get notified when it gets banned :police_officer:")
+        embed.add_field(name="feedback",value="More features will be added soon? :poop:")
         embed.add_field(name="Made by Raid7#1158",value="Version 1.1")
         await message.channel.send(content=None,embed=embed)
-    if message.content.find("!check") != -1:
+    if message.content.find("?check") != -1:
         await message.add_reaction('\N{THUMBS UP SIGN}')
         await message.channel.send(checkit(message.content))
-    if message.content.find("!notify") != -1:
+    if message.content.find("?notify") != -1:
         user = message.author
         await user.send("You will be notified when the account gets banned :bell:")
         await message.add_reaction('\N{THUMBS UP SIGN}')
